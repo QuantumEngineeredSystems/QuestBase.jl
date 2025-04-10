421314fca829a85c5e8bcc0b1008a463d80db568
Orjan Ameye <orjan.ameye@hotmail.com>
2025-04-10 22:31:38 +0200

    refactor: move get_Jacobian function to HarmonicBalance

--- src/utils.jl
+++ src/utils.jl
@@ -12,22 +12,6 @@ function show_fields(object)
     end
 end
 
-" Get the Jacobian of a set of equations `eqs` with respect to the variables `vars`. "
-function get_Jacobian(eqs::Vector{Num}, vars::Vector{Num})::Matrix{Num}
-    length(eqs) == length(vars) || error("Jacobians are only defined for square systems!")
-    M = Matrix{Num}(undef, length(vars), length(vars))
-
-    for idx in CartesianIndices(M)
-        M[idx] = expand_derivatives(d(eqs[idx[1]], vars[idx[2]]))
-    end
-    return M
-end # should replace with Symbolics.jacobian
-
-function get_Jacobian(eqs::Vector{Equation}, vars::Vector{Num})::Matrix{Num}
-    expr = Num[getfield(eq, :lhs) - getfield(eq, :rhs) for eq in eqs]
-    return get_Jacobian(expr, vars)
-end
-
 macro eqtest(expr)
     @assert expr.head == :call && expr.args[1] in [:(==), :(!=)]
     return esc(
